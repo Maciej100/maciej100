66163357f5c3416eafb82f90e63c7c20c25e8bf9
Maciej Sochan <44675203+Maciej100@users.noreply.github.com>
2018-11-06 17:00:23 +0000

    Update file from github.ps1

--- file from github.ps1
+++ file from github.ps1
@@ -3,4 +3,7 @@ if (-not(Test-Path -Path 'C:\temp'))
     New-Item -Path 'C:\temp' -ItemType directory
 }
 
-Set-Content -Path "C:\temp\file from github.txt" -Value "message"
+
+$date = Get-Date -Format ddmmyyyyhhmmss
+$filename = $date + ".txt"
+New-Item -Path C:\temp -Name $filename
