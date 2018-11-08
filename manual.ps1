5b60f01cf441f2fc56ed141dfd8dccfe3d89483f
Maciej Sochan <44675203+Maciej100@users.noreply.github.com>
2018-11-08 16:36:53 +0000

    Update manual.ps1

--- manual.ps1
+++ manual.ps1
@@ -15,5 +15,5 @@
 
 ### next time ####
 
-new-item -Path C:\temp -Name value_manual.txt
-#New-item -Path "C:\files\value manual.txt"
+#new-item -Path C:\temp -Name value_manual.txt
+New-item -Path "C:\temp\value manual.txt"
