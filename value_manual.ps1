82b58591561e7b1b791478d2f332da7064ca790b
Maciej Sochan <44675203+Maciej100@users.noreply.github.com>
2018-11-08 15:42:14 +0000

    Update value_manual.ps1

--- value_manual.ps1
+++ value_manual.ps1
@@ -4,5 +4,7 @@ if (-not(Test-Path -Path 'C:\temp'))
     New-Item -Path 'C:\temp' -ItemType directory
 }
 
+#$filename
+
 # Using the environment variables exposed by the Jenkins job 
 set-content -Path "C:\temp\$($env:Filename).txt" -Value $env:Message
