44a4c44ab2c4aeac1efc0d0876bb148e31af03af
Maciej Sochan <44675203+Maciej100@users.noreply.github.com>
2018-11-08 15:56:01 +0000

    Update and rename value_manual.ps1 to value manual.ps1

--- value manual.ps1
+++ value manual.ps1
@@ -7,9 +7,13 @@
 # Using the environment variables exposed by the Jenkins job 
 #set-content -Path "C:\temp\$($env:Filename).txt" -Value $env:Message
 
+#### next time #####
+
 #$date = Get-Date -Format ddmmyyyyhhmmss
 #$filename = $date + ".txt"
 #New-Item -Path C:\temp -Name $filename
 
-New-Item -Path C:\temp -Name filemanual.txt
+### next time ####
+
+New-Item -Path C:\temp -Name value manual.txt
 #New-item -Path "C:\files\value manual.txt"
