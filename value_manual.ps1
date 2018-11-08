11ca652351a661d214470b5754e5c74183bf9597
Maciej Sochan <44675203+Maciej100@users.noreply.github.com>
2018-11-08 15:47:54 +0000

    Update value_manual.ps1

--- value_manual.ps1
+++ value_manual.ps1
@@ -1,10 +1,15 @@
 # Create Temp Directory
-if (-not(Test-Path -Path 'C:\temp'))
-{
-    New-Item -Path 'C:\temp' -ItemType directory
-}
-
-$filename = "variable_name"
+#if (-not(Test-Path -Path 'C:\temp'))
+#{
+#    New-Item -Path 'C:\temp' -ItemType directory
+#}
 
 # Using the environment variables exposed by the Jenkins job 
-set-content -Path "C:\temp\$($env:Filename).txt" -Value $env:Message
+#set-content -Path "C:\temp\$($env:Filename).txt" -Value $env:Message
+
+#$date = Get-Date -Format ddmmyyyyhhmmss
+#$filename = $date + ".txt"
+#New-Item -Path C:\temp -Name $filename
+
+New-Item -Path C:\temp -Name filemanual.txt
+
