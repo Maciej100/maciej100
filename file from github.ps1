581436af07c58e3d1203a4f2bb7e1334784d83e6
Maciej Sochan <44675203+Maciej100@users.noreply.github.com>
2018-11-09 11:33:49 +0000

    Delete file from github.ps1

--- file from github.ps1
+++
@@ -1,9 +0,0 @@
-if (-not(Test-Path -Path 'C:\temp'))
-{
-    New-Item -Path 'C:\temp' -ItemType directory
-}
-
-
-$date = Get-Date -Format ddmmyyyyhhmmss
-$filename = $date + ".txt"
-New-Item -Path C:\temp -Name $filename
